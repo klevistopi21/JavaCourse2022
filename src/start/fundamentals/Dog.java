a78d911c98ecf0152201801d4346c54b13f14e3c
Klevis <klevistopi@gmail.com>
2022-04-26 10:25:49 +0200

    fundamentals

---
+++ src/start/fundamentals/Dog.java
@@ -0,0 +1,45 @@
+package start.fundamentals;
+
+public class Dog {
+    //karakteristika/vecori => variabla (instance)
+    //accessModifier, tipi, emri i variables, pikepresje
+    //accessModifier => public, private, protected, default
+
+    public String name; //by default = null
+    public int age; // by default = 0
+    public String type;
+    public String color;
+    public char gender; // by default = 0
+    public boolean isPet; //by default = false
+
+
+    //sjelljet => metoda
+    //access modifier, return type, emri i metodes, (parametra/ose jo), {}
+
+
+    public void bark() {
+        System.out.println("hum hum");
+    }
+
+    public void play() {
+        System.out.println("It's time to play");
+    }
+
+    public void eat() {
+        System.out.println("Stop playing, time to eat");
+    }
+
+    public void sleep() {
+        System.out.println("Finally time to sleep. Good night!");
+    }
+
+    public void printData() {
+        System.out.println("Emri eshte: " + name);
+        System.out.println("Mosha: " + age);
+        System.out.println("Lloji: " + type);
+        System.out.println("Ngjyra:" + color);
+        System.out.println("Gender: " + gender);
+        System.out.println("Eshte kafshe shtepiake?" + isPet);
+    }
+
+}
---
+++ src/start/fundamentals/Main.java
@@ -0,0 +1,32 @@
+package start.fundamentals;
+
+public class Main {
+    public static void main(String[] args) {
+        //Emri i klases, emri i objektit, =, new, Emri i klases, (parametra/ose jo)
+
+        Dog dog1 = new Dog();
+        dog1.name = "Bubi";
+        dog1.age = 3;
+        dog1.type = "X";
+        dog1.color = "white";
+        dog1.gender = 'F';
+        dog1.isPet = false;
+        dog1.printData();
+        dog1.bark();
+        dog1.play();
+        dog1.eat();
+        dog1.sleep();
+
+        Dog dog2 = new Dog();
+        dog2.name = "Tobi";
+        dog2.age = 1;
+        dog2.type = "Y";
+        dog2.color = "black";
+        dog2.gender = 'M';
+        dog2.isPet = true;
+        dog2.printData();
+        dog2.sleep();
+
+    }
+
+}
